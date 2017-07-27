fc98b27855ef400217e8dd95d9cd6ac424010605
andrzejjozefow <andrzej.jozefow@gmail.com>
2017-07-27 22:40:25 +0200

    Receipts feature added

--- src/Invoice.java
+++ src/Invoice.java
@@ -4,11 +4,13 @@ public class Invoice {
     private int id;
     private String tytul;
     private int kwota;
+    private int nip;
 
-    public Invoice(int id, String tytul, int kwota) {
+    public Invoice(int id, String tytul, int kwota, int nip) {
         this.id = id;
         this.tytul = tytul;
         this.kwota = kwota;
+        this.nip = nip;
     }
 
     public int getId() {
@@ -23,12 +25,15 @@ public class Invoice {
         return kwota;
     }
 
+    public int getNip() { return nip;}
+
     @Override
     public String toString() {
-        return "Invoice{" +
-                "id=" + id +
-                ", tytul='" + tytul + '\'' +
-                ", kwota=" + kwota +
-                '}';
+        return "\n" + "Invoice{" +
+                "id=" + id + "\n"+
+                "tytul='" + tytul + '\'' + "\n" +
+                "kwota=" + kwota + "\n" +
+                "nip=" + nip +
+                '}' + "\n";
     }
 }
