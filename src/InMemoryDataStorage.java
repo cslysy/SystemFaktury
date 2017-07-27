e9832a0de8fb616047d2abd2446a4a651c5bef03
andrzejjozefow <andrzej.jozefow@gmail.com>
2017-07-27 22:40:35 +0200

    Receipts feature added

--- src/InMemoryDataStorage.java
+++ src/InMemoryDataStorage.java
@@ -4,14 +4,25 @@ import java.util.List;
 public class InMemoryDataStorage implements DataStorage {
 
     private List<Invoice> invoices = new ArrayList<>();
+    private List<Receipt> receipts = new ArrayList<>();
 
+    //Invoices
     @Override
-    public void save(Invoice invoice){
+    public void saveInvoice(Invoice invoice){
         invoices.add(invoice);
     }
 
     @Override
     public List<Invoice> getAllInvoices(){
         return invoices;
+    }
+
+    //Receipts
+    @Override
+    public void saveReceipt(Receipt receipt) {receipts.add(receipt);}
+
+    @Override
+    public List<Receipt> getAllReceipts(){
+        return receipts;
     }
 }
