818907db0aa6ad639211126b237877c42bfdfeab
andrzejjozefow <andrzej.jozefow@gmail.com>
2017-07-27 22:40:51 +0200

    Receipts feature added

--- src/DataStorage.java
+++ src/DataStorage.java
@@ -3,7 +3,11 @@ import java.util.List;
 
 public interface DataStorage {
 
-    void save(Invoice invoice);
+    void saveInvoice(Invoice invoice);
 
     List<Invoice> getAllInvoices();
+
+    void saveReceipt(Receipt receipt);
+
+    List<Receipt> getAllReceipts();
 }
